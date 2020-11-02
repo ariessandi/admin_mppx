e22e4d15b08b6f8031a577d6df7738dbefaa9b30
ariessandi <ariessandi7@gmail.com>
2020-11-02 10:40:20 +0700

    Update globals.php
    update global

--- application/config/globals.php
+++ application/config/globals.php
@@ -8,6 +8,11 @@ $config['api_instansi']= 'http://localhost:8000/api/instansi';
 $config['api_user']= 'http://localhost:8000/api/core-user';
 $config['api_gallery']='http://localhost:8000/api/gallery';
 $config['api_auth']='http://localhost:8000/api/auth-coreuser/process';
-$config['api_Counter']='http://localhost:8000/api/counter';
+$config['api_counter']='http://localhost:8000/api/counter';
+$config['api_testi']='http://localhost:8000/api/testimony';
+$config['api_tkami']='http://localhost:8000/api/page/tentang-kami';
+$config['api_Fasilitas']='http://localhost:8000/api/page/fasilitas';
+$config['api_Survey']='http://localhost:8000/api/survey-question';
+
 
 ?>
